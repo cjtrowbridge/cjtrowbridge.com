fbff0306aa2407f92d6767562d0c71a8ab8b4828
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-04-29 22:42:19 -0700

    Update index.php
    Upgraded to Bootstrap 4!

--- resume/work_samples/index.php
+++ resume/work_samples/index.php
@@ -4,18 +4,21 @@
 <head>
 	<title>Work Samples | Resume | CJ Trowbridge</title>
 	
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-	<link rel="stylesheet" 
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" 
-	integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" 
-	crossorigin="anonymous">
-	<link rel="stylesheet" 
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" 
-	integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" 
-	crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" 
-	integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" 
-	crossorigin="anonymous"></script>
+	<!-- Required meta tags always come first -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+	
+	<link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<meta name="theme-color" content="#ffffff">
+	
+	<link rel="stylesheet" href="https://cjtrowbridge.com/css/style.css">
 	
 	<style>
 		th, td{
@@ -26,103 +29,80 @@
 		tr td:not(:last-of-type){
 			white-space: nowrap;
 		}
-	</style>	
-	
-	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.25.0/css/theme.default.min.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.25.0/js/jquery.tablesorter.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.25.0/js/jquery.tablesorter.widgets.min.js"></script>
-	
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
-	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#ffffff">
+	</style>
 </head>
 
 <body>
-<div class="container" id="wrapper">
-	<div class="col-md-12" id="main">
-		<h1>Work Samples</h1>
-		<a href="/resume">&lt;&lt; Back To Resume</a><br>
-		<a href="/">&lt;&lt; Back To Home Page</a>
-		
-		<div id="content_marketing"></div>
-		<h3>Content Marketing Examples</h3>
-		<div class="table-responsive">
-			<table class="table-striped">
-				<thead>
-					<tr>
-						<th>Link</th>
-						<th>Content Type</th>
-						<th>Description</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2016-02-26_-_Good_Things_Come_In_Threes.PNG" target="_blank">2016-02-26 - Good Things Come In Threes</a></td>
-						<td>Email Marketing</td>
-						<td>
-							<p>When  business is slow, big discounts can help drive growth.</p>
-						</td>
-					</tr>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2015-12-18 - Holiday Savings.pdf" target="_blank">2015-12-18 - Holiday Savings.pdf</a></td>
-						<td>Email Marketing</td>
-						<td>
-							<p>Many newsletters are driven by current events and holidays. These times are great for sales and promotions.</p>
-							<p>Leveraging sales and promotions effectively is all about communicating simple value propositions.</p>
-						</td>
-					</tr>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2015-11-05 - Tech 2U Arrested.PNG" target="_blank">2015-11-05 - Tech 2U Arrested.PNG</a></td>
-						<td>Social Media</td>
-						<td>Sometimes funny does it. This campaign came from an improvised photo taken at a street fair downtown while doing some presence marketing and canvassing.</td>
-					</tr>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2015-10-07 - Win10 amp Carbonite.pdf" target="_blank">2015-10-07 - Win10 & Carbonite.pdf</a></td>
-						<td>Email Marketing</td>
-						<td></td>
-					</tr>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2015-07-29 - Reserve Your FREE Copy of Windows 10 Today.pdf" target="_blank">2015-07-29 - Reserve Your FREE Copy of Windows 10 Today.pdf</a></td>
-						<td>Email Marketing</td>
-						<td>This campaign leverages current events to drive inbound sales and create engagement around sales opportunities.</td>
-					</tr>
-					<tr>
-						<td><a href="/resume/work_samples/content_marketing/2013-11-27 - We Fix Any Tech Problem.PNG" target="_blank">2013-11-27 - We Fix Any Tech Problem.PNG</a></td>
-						<td>Social Media</td>
-						<td>This is an example of great, engaging content; I found a charismatic employee and added a simple message, "We Fix Any Tech Issue."</td>
-					</tr>
-				</tbody>
-			</table>
+	<div class="container">
+		<div class="col-md-12">
+			<h1>Work Samples</h1>
+			<a href="/resume">&lt;&lt; Back To Resume</a><br>
+			<a href="/">&lt;&lt; Back To Home Page</a>
+			
+			<div id="content_marketing">&nbsp;</div>
+			<h3>Content Marketing Examples</h3>
+			<div class="table-responsive">
+				<table class="table-striped">
+					<thead>
+						<tr>
+							<th>Link</th>
+							<th>Content Type</th>
+							<th>Description</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2016-02-26_-_Good_Things_Come_In_Threes.PNG" target="_blank">2016-02-26 - Good Things Come In Threes</a></td>
+							<td>Email Marketing</td>
+							<td>
+								<p>When  business is slow, big discounts can help drive growth.</p>
+							</td>
+						</tr>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2015-12-18 - Holiday Savings.pdf" target="_blank">2015-12-18 - Holiday Savings.pdf</a></td>
+							<td>Email Marketing</td>
+							<td>
+								<p>Many newsletters are driven by current events and holidays. These times are great for sales and promotions.</p>
+								<p>Leveraging sales and promotions effectively is all about communicating simple value propositions.</p>
+							</td>
+						</tr>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2015-11-05 - Tech 2U Arrested.PNG" target="_blank">2015-11-05 - Tech 2U Arrested.PNG</a></td>
+							<td>Social Media</td>
+							<td>Sometimes funny does it. This campaign came from an improvised photo taken at a street fair downtown while doing some presence marketing and canvassing.</td>
+						</tr>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2015-10-07 - Win10 amp Carbonite.pdf" target="_blank">2015-10-07 - Win10 & Carbonite.pdf</a></td>
+							<td>Email Marketing</td>
+							<td></td>
+						</tr>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2015-07-29 - Reserve Your FREE Copy of Windows 10 Today.pdf" target="_blank">2015-07-29 - Reserve Your FREE Copy of Windows 10 Today.pdf</a></td>
+							<td>Email Marketing</td>
+							<td>This campaign leverages current events to drive inbound sales and create engagement around sales opportunities.</td>
+						</tr>
+						<tr>
+							<td><a href="/resume/work_samples/content_marketing/2013-11-27 - We Fix Any Tech Problem.PNG" target="_blank">2013-11-27 - We Fix Any Tech Problem.PNG</a></td>
+							<td>Social Media</td>
+							<td>This is an example of great, engaging content; I found a charismatic employee and added a simple message, "We Fix Any Tech Issue."</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<!--div id="enterprise_development"></div>
+			<h3>Enterprise Development</h3>
+			<ul>
+				<li></li>
+			</ul-->
 		</div>
-		<!--div id="enterprise_development"></div>
-		<h3>Enterprise Development</h3>
-		<ul>
-			<li></li>
-		</ul-->
 	</div>
-</div>
-
-<script>
-	$('.tablesorter').tablesorter();
-</script>
+	<!-- jQuery first, then Bootstrap JS. -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+	
+	<script>
+		$('.tablesorter').tablesorter();
+	</script>
 </body>
 
 </html>
