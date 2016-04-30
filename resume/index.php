bafe6d52daa370a24a19c7f7105c3c3af88f178e
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-04-29 22:34:46 -0700

    Update index.php
    Updated to Bootstrap 4

--- resume/index.php
+++ resume/index.php
@@ -4,104 +4,91 @@
 <head>
 	<title>Resume | CJ Trowbridge</title>
 	
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-	<link rel="stylesheet" 
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" 
-	integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" 
-	crossorigin="anonymous">
-	<link rel="stylesheet" 
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" 
-	integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" 
-	crossorigin="anonymous">
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" 
-	integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" 
-	crossorigin="anonymous"></script>
-	
-	<link rel="stylesheet" 
-	href="/main.css">
+	<!-- Required meta tags always come first -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
 	
-	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 	
+	<link rel="stylesheet" href="https://cjtrowbridge.com/css/style.css">
 	
 </head>
 
 <body>
-<div class="container" id="wrapper">
-	<div class="col-md-12" id="main">
-		<h1>Resume and Industry Awards</h1>
-		<a href="/">&lt;&lt; Back To Home Page</a>
-		
-		<div id="tech_2u"></div>
-		<h3>Tech 2U</h3>
-		<strong>Enterprise Development</strong>
-		<em>April 2013-Present</em>
-		<ul>
-			<li>Brought in hundreds of thousands of dollars in new revenue;</li>
-			<li>Created a position for myself developing and expanding a diverse, multiregional business</li>
-			<li>My Enterprise Development work has produced well over <strong>$200,000 in new revenue</strong>, growing to bring in over <b>$50k/month</b> by building employee development around my new real-time KPI leaderboards and incentives</li>
-			<li>My <a href="./work_samples/" target="_blank">content marketing work</a> has brought in almost <strong>$80k in revenue</strong> to date through email marketing, social media and direct mail</li>
-			<li>I built an outreach team and a platform which has brought in over <strong>$78k in revenue</strong> to date</li>
-			<li>Co-manage all daily operations and logistics for the company along with a leadership team, and assist with inbound and outbound sales</li>
-			<li>Marketing implementation <strong>cost</strong> <strong>savings of 31.3%</strong> over the company's previous marketing and outreach programs</li>
-		</ul>
-		<div id="sequoia_equities"></div>
-		<h3>Sequoia Equities</h3>
-		<strong>Developer, "Innovation Champion" Award Winner</strong>
-		<em>2011 - 2012</em>
-		<ul>
-			<li>Transformed a failed cafe into a thriving business</li>
-			<li>Earned Sequoia's "Innovation Champion" award for 2012</li>
-			<li>Contracted during a large equities corporation's takeover of several dormitories with lots of room for development and revenue growth</li>
-			<li>In charge of developing and reopening a cafe and several new mobile food carts with the goal of effectively, efficiently targeting premium, niche markets</li>
-			<li>Developed significant relationships with customers and vendors like Starbucks and Sysco</li>
-			<li>Developed inventory management, company culture, and efficient operations</li>
-			<li>My efforts led to <strong>growth of 1600%</strong> in the cafe over historic data. Instead of operating in the red with an average of just $50 per day in revenue, it was now bringing in close to <strong>$800 revenue per day</strong>, with about a 50% profit margin</li>
-			<li>My new food carts brought in an average of <strong>$2,000 per week</strong>, with 75% of that being profit</li>
-		</ul>
-		<div id="starbucks_corporation"></div>
-		<h3>Starbucks Corporation</h3>
-		<strong>Customer Service, Leadership</strong>
-		<em>2008 - 2014</em>
-		<ul>
-			<li>Responsible for several advanced internal roles including leading roles including <b>Playbook</b> and the <b>Coffee Master</b> program</li>
-			<li>Responsible for management and leadership, training new employees, cash-handling, and using product knowledge to identify and create sales opportunities, while creating enthusiastically satisfied customer experiences at some of the busiest stores in the country in Portland and Sacramento.</li>
-			<li>My most recent advanced project was leading the regional implementation of a new scaled labor algorithm program called "<b>Playbook</b>." This program was a radical change for the company which called on those of us with excellent leadership skills to execute big changes across the world simultaneously. I was responsible for conducting most of the initial training for the Sacramento region, and following-up with hundreds of employees in different locations to verify the program was being deployed correctly across the area.</li>
-			<li>I was also a <strong>Coffee Master</strong> which is a leadership program focused on the education of customers and employees in the more subtle and advanced aspects of the products and processes involved in the culture that Starbucks wants to create. I taught many seminars and workshops on methods and best practices for using Starbucks products as well as understanding the global economics and social concerns involved and how they influence the final product.</li>
-			<li>Through these programs, I am certified through Seattle City University, and the people I train are eligible for transferable college credits after taking my classes and seminars.</li>
-		</ul>
+	<div class="container">
+		<div class="row">
+			&nbsp;
+		</div>
+		<div class="row">
+			<div class="col-md-12" id="main">
+				<h1>Resume and Industry Awards</h1>
+				<a href="/">&lt;&lt; Back To Home Page</a>
+				
+				<div id="tech_2u"></div>
+				<h3>Tech 2U</h3>
+				<strong>Enterprise Development</strong>
+				<em>April 2013-Present</em>
+				<ul>
+					<li>Brought in hundreds of thousands of dollars in new revenue;</li>
+					<li>Created a position for myself developing and expanding a diverse, multiregional business</li>
+					<li>My Enterprise Development work has produced well over <strong>$200,000 in new revenue</strong>, growing to bring in over <b>$50k/month</b> by building employee development around my new real-time KPI leaderboards and incentives</li>
+					<li>My <a href="./work_samples/" target="_blank">content marketing work</a> has brought in almost <strong>$80k in revenue</strong> to date through email marketing, social media and direct mail</li>
+					<li>I built an outreach team and a platform which has brought in over <strong>$78k in revenue</strong> to date</li>
+					<li>Co-manage all daily operations and logistics for the company along with a leadership team, and assist with inbound and outbound sales</li>
+					<li>Marketing implementation <strong>cost</strong> <strong>savings of 31.3%</strong> over the company's previous marketing and outreach programs</li>
+				</ul>
+				<div id="sequoia_equities"></div>
+				<h3>Sequoia Equities</h3>
+				<strong>Developer, "Innovation Champion" Award Winner</strong>
+				<em>2011 - 2012</em>
+				<ul>
+					<li>Transformed a failed cafe into a thriving business</li>
+					<li>Earned Sequoia's "Innovation Champion" award for 2012</li>
+					<li>Contracted during a large equities corporation's takeover of several dormitories with lots of room for development and revenue growth</li>
+					<li>In charge of developing and reopening a cafe and several new mobile food carts with the goal of effectively, efficiently targeting premium, niche markets</li>
+					<li>Developed significant relationships with customers and vendors like Starbucks and Sysco</li>
+					<li>Developed inventory management, company culture, and efficient operations</li>
+					<li>My efforts led to <strong>growth of 1600%</strong> in the cafe over historic data. Instead of operating in the red with an average of just $50 per day in revenue, it was now bringing in close to <strong>$800 revenue per day</strong>, with about a 50% profit margin</li>
+					<li>My new food carts brought in an average of <strong>$2,000 per week</strong>, with 75% of that being profit</li>
+				</ul>
+				<div id="starbucks_corporation"></div>
+				<h3>Starbucks Corporation</h3>
+				<strong>Customer Service, Leadership</strong>
+				<em>2008 - 2014</em>
+				<ul>
+					<li>Responsible for several advanced internal roles including leading roles including <b>Playbook</b> and the <b>Coffee Master</b> program</li>
+					<li>Responsible for management and leadership, training new employees, cash-handling, and using product knowledge to identify and create sales opportunities, while creating enthusiastically satisfied customer experiences at some of the busiest stores in the country in Portland and Sacramento.</li>
+					<li>My most recent advanced project was leading the regional implementation of a new scaled labor algorithm program called "<b>Playbook</b>." This program was a radical change for the company which called on those of us with excellent leadership skills to execute big changes across the world simultaneously. I was responsible for conducting most of the initial training for the Sacramento region, and following-up with hundreds of employees in different locations to verify the program was being deployed correctly across the area.</li>
+					<li>I was also a <strong>Coffee Master</strong> which is a leadership program focused on the education of customers and employees in the more subtle and advanced aspects of the products and processes involved in the culture that Starbucks wants to create. I taught many seminars and workshops on methods and best practices for using Starbucks products as well as understanding the global economics and social concerns involved and how they influence the final product.</li>
+					<li>Through these programs, I am certified through Seattle City University, and the people I train are eligible for transferable college credits after taking my classes and seminars.</li>
+				</ul>
 
-		<h3>Volunteer Work</h3>
-		<em>2013-2015</em>
-		<ul>
-			<li>Ongoing volunteer work with <a href="http://www.standupplacer.org/" target="_blank">Stand Up Placer</a></li>
-		</ul>
-		<h3>Other Skills</h3>
-		<ul>
-			<li>Leading and implementing change in a corporate environment</li>
-			<li>Providing excellent customer service</li>
-			<li>Learning quickly in new environments</li>
-		</ul>
+				<h3>Volunteer Work</h3>
+				<em>2013-2015</em>
+				<ul>
+					<li>Ongoing volunteer work with <a href="http://www.standupplacer.org/" target="_blank">Stand Up Placer</a></li>
+				</ul>
+				<h3>Other Skills</h3>
+				<ul>
+					<li>Leading and implementing change in a corporate environment</li>
+					<li>Providing excellent customer service</li>
+					<li>Learning quickly in new environments</li>
+				</ul>
 
+			</div>
+		</div>
 	</div>
-</div>
+	<!-- jQuery first, then Bootstrap JS. -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 </body>
 
 </html>
