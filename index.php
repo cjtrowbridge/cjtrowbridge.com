18644746246c0e6a3d4b906166d59e27b90de74a
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-04-29 21:44:34 -0700

    Update index.php
    Upgraded to latest version of Bootstrap and JQuery.

--- index.php
+++ index.php
@@ -4,62 +4,60 @@
 <head>
 	<title>CJ Trowbridge</title>
 	
-	<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Required meta tags always come first -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+	
 	<link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 	<meta name="theme-color" content="#ffffff">
 	
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
-
-	<link rel="stylesheet" href="//cjtrowbridge.com/css/cjtrowbridge.com.css">
+	<link rel="stylesheet" href="https://cjtrowbridge.com/css/style.css">
 </head>
 
 <body>
-<div class="container" id="wrapper">
-	<div class="col-md-12" id="main">
-		<div class="col-md-12">
-			&nbsp;
-		</div>
-		<!--div class="col-xs-12" id="calendar">
-			<iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;mode=WEEK&amp;height=700&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=chris.j.trowbridge%40gmail.com&amp;color=%23182C57&amp;src=8ms65nok27tsjug6h77mfrio1s%40group.calendar.google.com&amp;color=%23333333&amp;src=80sllsknaj4sjrnkgd6p312ha0%40group.calendar.google.com&amp;color=%231B887A&amp;src=ctrowbridge%40ts2u.com&amp;color=%23125A12&amp;src=4pmpqh08haf5vfr16uoov22qd8%40group.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FLos_Angeles" style="border-width:0;width:100%;height:700px" frameborder="0" scrolling="no"></iframe><br><br>
-		</div-->
-		<div class="col-md-4 col-sm-12">
-			<img src="/cj.jpg" alt="Photo of CJ Trowbridge">
-		</div>
-		<div class="col-md-8 col-sm-12">
-			<h2>I'm CJ Trowbridge.</h2>
-			<ul>
-				<li>
-					I am a tenth-generation American with a family <a href="//trowbridge.house" target="_blank">history</a> of 4 
-					centuries of American entrepreneurship and humanitarianism, and English entrepreneurship before that. We share a 
-					love of travel and adventure, along with a passion for tackling new problems and helping people.
-				</li>
-				<li>
-					My projects and career have taken me though many fields from building a cafe to creating ecommerce websites for 
-					small businesses. I am currently delving into intrapreneurship with <a href="//tech2u.com" target="_blank">Tech 2U</a>, 
-					leading Enterprise Development.
-				</li>
-				<li>
-					The best way to get ahold of me is by email at <a href="mailto:chris.j.trowbridge@gmail.com">chris.j.trowbridge@gmail.com</a>.
-				</li>
-			</ul>
+	<div class="container"></div>
+		<div class="col-md-12" id="main">
+			<div class="col-md-4 col-sm-12">
+				<img src="/cj.jpg" alt="Photo of CJ Trowbridge">
+			</div>
+			<div class="col-md-8 col-sm-12">
+				<h2>I'm CJ Trowbridge.</h2>
+				<ul>
+					<li>
+						I am a tenth-generation American with a family <a href="//trowbridge.house" target="_blank">history</a> of 4 
+						centuries of American entrepreneurship and humanitarianism, and English entrepreneurship before that. We share a 
+						love of travel and adventure, along with a passion for tackling new problems and helping people.
+					</li>
+					<li>
+						My projects and career have taken me though many fields from building a cafe to creating ecommerce websites for 
+						small businesses. I am currently delving into intrapreneurship with <a href="//tech2u.com" target="_blank">Tech 2U</a>, 
+						leading Enterprise Development.
+					</li>
+					<li>
+						The best way to get ahold of me is by email at <a href="mailto:chris.j.trowbridge@gmail.com">chris.j.trowbridge@gmail.com</a>.
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="col-md-12 footer">
-	Check out my 
-		<a href="https://github.com/cjtrowbridge/" target="_blank">github</a>, 
-		<!--a href="javascript:void(0);" onclick="$('#calendar').slideToggle();">calendar</a>, -->
+	<div class="col-md-12 footer">
+		Check out my 
+		<a href="https://github.com/cjtrowbridge/" target="_blank">github</a>,
 		<a href="//www.linkedin.com/in/cjtrowbridge" target="_blank">linkedin</a>, 
 		<a href="//cjtrowbridge.com/resume/">resume</a>, 
-		and 
-		<a href="//blog.cjtrowbridge.com" target="_blank">blog</a>.
-</div>
+		and <a href="//blog.cjtrowbridge.com" target="_blank">blog</a>.
+	</div>
+	
+	<!-- jQuery first, then Bootstrap JS. -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
 </body>
 
 </html>
