7254b393accc6f59811e705c59b36317c4b03e43
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2016-08-22 10:23:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"-->
-	<link rel="icon" type="image/jpg href="/cj.jpg">
+	<link rel="icon" type="image/jpg" href="/cj.jpg">
 	<meta name="theme-color" content="#ffffff">
 	
 	<link rel="stylesheet" href="https://cjtrowbridge.com/css/style.css">
