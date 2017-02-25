4f2d84735f028dcacaa9bcd812232e5c77f327db
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-02-25 11:43:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ function round_up($number, $precision = 2)
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-	<link rel="icon" type="image/jpg" href="/cj.jpg">
+	<link rel="icon" type="image/jpg" href="/christopher.jpg">
 	<link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
