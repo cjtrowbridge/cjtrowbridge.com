07fc52880dae5fb1ddf7a78aa27e7d83ab92d8f0
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-27 12:46:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -52,6 +52,7 @@ function round_up($number, $precision = 2){
 							
 						</ol>
 					</p>
+					<hr>
 					<p>I am also currently exploring exotic income sources at <a href="https://exoticweapons.io/" target="_blank">ExoticWeapons.io</a>.</p>
 					<p>I was born a <a href="//trowbridge.house" target="_blank">tenth generation</a> American entrepreneur, in the mountains of Oregon. With my life expectancy, I am <?php echo round((time()-599373240) / (60*60*24*365*78)*100); ?> percent through, and have <?php echo round(((599373240+60*60*24*365*78)-time())/(60*60*24*365),1); ?> years left.</p>
 				</div>
