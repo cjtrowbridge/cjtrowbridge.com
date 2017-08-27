d28179beb0fee0f662b0f9235a69e33ec3974de2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-08-27 15:33:41 -0700

    Update index.php

--- index.php
+++ index.php
@@ -44,11 +44,11 @@ function round_up($number, $precision = 2){
 							<li><b><a href="https://condensr.io/" target="_blank">Condensr</a>:</b> Condense longform text.</li>
 							<li><b><a href="https://exoticweapons.io/" target="_blank">Exotic Weapons</a>:</b> A blog about building passive income sources.</li>
 							<li><b><a href="https://topstoryreview.com/" target="_blank">Top Story Review</a>: [RC-1]</b> An automatic parser and condenser of top news sources.</li>
+							<li><b><a href="javascript:void(0);">Captain's Log</a>: [ALPHA]</b> A journaling tool.</li>
 							<li><b><a href="javascript:void(0);">Wearing Today</a>: [ALPHA]</b> Track what you wear on each day so you don't look like a schmuck wearing the same thing twice to a class or date.</li>
 							<li><b><a href="javascript:void(0);">Little John</a>: [Pre-ALPHA]</b> A simple cryptocurrency trading app. <br><i><a href="https://en.wikipedia.org/wiki/Little_John" target="_blank">"Little John is a legendary fellow outlaw of Robin Hood."</a></i></li>
 							<li><b><a href="javascript:void(0);">Smart Mailer</a>: [Pre-ALPHA]</b> A simple marketing automation platform which integrates with existing databases and automatically sends emails.</li>
 							<li><b><a href="javascript:void(0);">Subscriptions as a Service</a>: [ALPHA]</b> Manage user subscriptions simply. We handle everything for you with simple tools.</li>
-							<li><b><a href="javascript:void(0);">Powerhouse CRM</a>: [ALPHA]</b> A powerful and free CRM platform.</li>
 						</ol>
 					</p>
 					<hr>
