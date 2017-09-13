5b69c9923fd1cbce035284ecac72ac204e9fa20d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-09-13 16:56:45 -0700

    Update update-photo.php

--- update-photo.php
+++ update-photo.php
@@ -1,9 +1,9 @@
 <?php
 
-if(date('Y-m-d',filemtime('cj.jpg'))==date(Y-m-d)){
+if(date('Y-m-d',filemtime('cj.jpg'))==date('Y-m-d')){
   die('Too soon!');
 }
 
 copy("https://graph.facebook.com/540290721/picture?width=9999&height=9999", "cj.jpg");
 
-die('ok. Previous update was '.date('Y-m-d',filemtime('cj.jpg')));
+die('ok');
