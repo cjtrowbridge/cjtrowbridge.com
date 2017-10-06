226632aa2b88bafcecf9abd0c97dab696f5e0ad4
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-05 21:59:35 -0700

    Update index.php

--- goals/index.php
+++ goals/index.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
   
-  <stlye>
+  <style>
     body{
       background-color: #c7c7c7;
     }
