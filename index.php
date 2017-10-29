7ad28a4d5421983624ca587ee97dcc6ec52d6bc2
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-10-29 12:53:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ function round_up($number, $precision = 2){
                   <h1 style="white-space: nowrap;">CJ <a href="https://trowbridge.house/" style="color: #000;" target="_blank">Trowbridge</a></h1>
                   <p><b>Software Engineer & Entrepreneur</b><br><i><a href="/resume">Award-winning</a> innovator, building <a href="https://twitter.com/gonedark/status/898606946542120960" target="_blank">technical solutions</a> to business problems.</i></p>
                   <hr>
-                  <b>Current Academic Goals:</b>
+                  Current Academic Goals:
                   <p>
                     <ul>
                       <li>Associate's in Social Justice</li>
@@ -60,8 +60,6 @@ function round_up($number, $precision = 2){
                       <li>Master's in Business Administration</li>
                     </ul>
                   </p>
-                
-                  <p><b>Current Projects:</b></p>
                   
                   <div class="row no-gutters">
                     <div class="col-xs-12 col-md-10">
