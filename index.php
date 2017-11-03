1f2dca44efcedfad8bbb5fa5aadb6e8c71680e83
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-03 16:34:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ function round_up($number, $precision = 2){
                   <div class="blockquote">"To defend and preserve the individual rights and liberties guaranteed to every person in this country by the Constitution and laws of the United States."</div>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                  <a href="https://action.aclu.org/secure/aclu-first-line-defense-peoples-rights?s_src=UNV170101INA&ms=web_hero_trump_gol" target="_blank">Pitch In a Few Bucks a Month</a>
+                  <a href="https://action.aclu.org/secure/aclu-first-line-defense-peoples-rights?s_src=UNV170101INA&ms=web_hero_trump_gol" target="_blank" class="btn btn-block btn-success">Pitch In a Few Bucks a Month</a>
                 </div>
                 
                 <div class="col-xs-12"><br></div>
@@ -115,7 +115,7 @@ function round_up($number, $precision = 2){
                   <div class="blockquote">"To inspire the people of Earth to explore other worlds, understand our own, and seek life elsewhere."</div>
                 </div>
                 <div class="col-xs-12 col-md-4">
-                  <a href="https://secure.planetary.org/site/SPageNavigator/memberships.html" target="_blank">Pitch In a Few Bucks a Month</a>
+                  <a href="https://secure.planetary.org/site/SPageNavigator/memberships.html" target="_blank" class="btn btn-block btn-success">Pitch In a Few Bucks a Month</a>
                 </div>
                 
               </div>
