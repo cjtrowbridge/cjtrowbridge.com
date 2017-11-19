26953bc439caf66062ad067810c3a5df9ee5f775
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2017-11-19 15:47:52 -0800

    Update index.php

--- goals/index.php
+++ goals/index.php
@@ -56,6 +56,12 @@ for ($i = 0 ; $i < 6; $i++){
               $HTML = stristr($HTML,'<main');
               $EndPos = strpos($HTML,'</main>');
               $HTML = substr($HTML,0,$EndPos);
+              
+              $HTML = stristr($HTML,'<div class="entry-content">');
+              $EndPos = strpos($HTML,'<div class="entry-content">'.PHP_EOL.'			</div><!-- .entry-content -->');
+              $HTML = substr($HTML,0,$EndPos);
+              
+              
               echo $HTML;
             ?>
           </div>
