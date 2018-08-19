200aaa5bf080e965dab6afe28668979698b631f7
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-08-19 14:33:55 -0700

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,16 @@ function Burndown($Time){
 ?><!DOCTYPE html>
 <html>
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-49854332-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-49854332-1');
+  </script>
+
   <title>CJ Trowbridge</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
