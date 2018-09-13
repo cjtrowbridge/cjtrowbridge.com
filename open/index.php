ab2d98f8437a54fa062c76834a6948132e68095c
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-12 17:09:28 -0700

    Update index.php

--- open/index.php
+++ open/index.php
@@ -22,7 +22,7 @@
     
       <div class="card">
         <div class="card-body">
-          <iframe width="600" height="304.5" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&amp;format=interactive"></iframe>
+          <iframe style="width: 100%;" width="600" height="304.5" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&amp;format=interactive"></iframe>
         </div>
       </div>
       
