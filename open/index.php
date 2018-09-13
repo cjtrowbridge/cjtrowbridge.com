ab90b1e7b758f73b0c5c3a420e82f8d71bb89f80
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-12 17:35:18 -0700

    Update index.php

--- open/index.php
+++ open/index.php
@@ -11,7 +11,13 @@
 
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-
+  <style>
+    img{
+      width: 100%;
+      max-width: 100%;
+    }
+  </style<
+  
 </head>
 <body>
 
