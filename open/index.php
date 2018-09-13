d434918938a88dcbed9b55eed9012e0b9e12ceb3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-13 14:28:14 -0700

    Update index.php

--- open/index.php
+++ open/index.php
@@ -30,13 +30,15 @@
       <div class="row">
         <div class="col-12 col-md-6">
           <p class="text-center">
-            <img src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&format=image" alt="Revenue Mix">
+            <img src="/img/revenue_totals.jpg" alt="Revenue Mix">
+            <!--img src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&format=image" alt="Revenue Mix"-->
             <!--iframe width="800" height="304.5" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&amp;format=image"></iframe-->
           </p>
         </div><!--/col-12 col-md-6-->
         <div class="col-12 col-md-6">
           <p class="text-center">
-            <img src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=1236350701&format=image" alt="Revenue Totals">
+            <img src="/img/revenue_totals.jpg" alt="Revenue Totals">
+            <!--img src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=1236350701&format=image" alt="Revenue Totals"-->
             <!--iframe width="800" height="303.5" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=1236350701&amp;format=image"></iframe-->
           </p>
         </div><!--/col-12 col-md-6-->
