ef01a9e6af88e5f1d19771a9e851c54f78840e3d
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2018-09-29 16:41:40 -0700

    Create index.php

---
+++ projects/ip/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$ip = $_SERVER['REMOTE_ADDR'];
+if(!(is_dir('cache'))){
+  mkdir('cache');
+}
+
+$cacheFile='cache/'.$ip.'.txt';
+if(file_exists($cacheFile)){
+  $data = file_get_contents($cacheFile);
+  if(isset($_GET['verbose'])){echo '<p>Found in cache.</p>';}
+}else{
+  $data = file_get_contents("http://ipinfo.io/{$ip}/json");
+  file_put_contents($cacheFile,$data);
+  if(isset($_GET['verbose'])){echo '<p>Fetched from API.</p>';}
+}
+
+$details = json_decode($data);
+echo '<h1>'.$details->city.'</h1>';
+echo '<pre>';
+var_dump($details);
+echo '</pre>';
