95cd852dd123b20f0528f52562ed7c6e83dac8c9
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-02-28 21:02:02 -0800

    Update update-media.php

--- update-media.php
+++ update-media.php
@@ -15,6 +15,14 @@ function UpdateMedia($URL,$File){
   echo '<p>Updated: <a href="'.$File.'">'.$File.'</a></p>';
 }
 
+function (){
+  $Sheet = file_get_contents("https://spreadsheets.google.com/feeds/list/1QiHfopOcjt34zO2PIvi4EpiD7ukdQ-7e4a5P9tYJIcs/od6/public/values?alt=json-in-script");
+  
+  $Cells = $Sheet->feed->entry;
+  
+  var_dump($Cells);
+}
+
 UpdateMedia("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&format=image", "img/revenue_mix.jpg");
 UpdateMedia("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=1236350701&format=image", "img/revenue_totals.jpg");
 
@@ -23,3 +31,4 @@ UpdateMedia("https://graph.facebook.com/540290721/picture?width=9999&height=9999
 UpdateMedia("https://graph.facebook.com/643280183/picture?width=9999&height=9999", "img/ben.jpg");
 UpdateMedia("https://graph.facebook.com/1227603033/picture?width=9999&height=9999", "img/zach.jpg");
 UpdateMedia("https://graph.facebook.com/797969965/picture?width=9999&height=9999", "img/jenny.jpg");
+
