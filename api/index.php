d20ae42b92f9d20e4abfa9c8b60629cd7f1b1c4b
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-09-05 15:22:24 -0700

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+  CJ Trowbridge
+  2019-09-05
+  
+  The purpose of this api is to deliver various non-static data to various apps and tools throughout this and other sites.
+
+*/
+
+require('Config.php');
