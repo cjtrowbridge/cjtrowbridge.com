2fe0a25b3560fcc3aa121a50e96d42444f84eda6
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-09-05 15:35:28 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -13,6 +13,6 @@ require('Config.php');
 
 if(isset($_GET['countTodoToday'])){
   //$Account = file_get_contents('https://api.trello.com/1/members/me/boards?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
-  $Todo = file_get_contents('https://api.trello.com/1/members/me/boards/'.$TrelloTodoBoardID.'?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  $Todo = file_get_contents('https://api.trello.com/1/members/me/boards/'.$TrelloTodoBoardID.'/?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
   echo $Todo;
 }
