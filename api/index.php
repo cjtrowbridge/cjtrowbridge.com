f511b9cd0eb8b81f8fa152cd75392a096d93e043
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2019-09-05 15:49:15 -0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -13,6 +13,16 @@ require('Config.php');
 
 if(isset($_GET['countTodoToday'])){
   //$Account = file_get_contents('https://api.trello.com/1/members/me/boards?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
-  $Todo = file_get_contents('https://api.trello.com/1/boards/'.$TrelloTodoBoardID.'/?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
-  echo $Todo;
+  //$Todo = file_get_contents('https://api.trello.com/1/boards/'.$TrelloTodoBoardID.'/?key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  //$TodoLists = file_get_contents('https://api.trello.com/1/boards/'.$TrelloTodoBoardID.'/?lists=open&key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  $TodoToday = file_get_contents('https://api.trello.com/1/lists/'.$TrelloTodoTodayListID.'/cards?fields=name&key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  $TodoThisweek = file_get_contents('https://api.trello.com/1/lists/'.$TrelloTodoThisweekListID.'/cards?fields=name&key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  $TodoNextweek = file_get_contents('https://api.trello.com/1/lists/'.$TrelloTodoNextWeekListID.'/cards?fields=name&key='.$TrelloAPIKey.'&token='.$TrelloAPIToken);
+  
+  $TodoToday    = json_decode($TodoToday,true);
+  $TodoThisweek = json_decode($TodoThisweek,true);
+  $TodoNextweek = json_decode($TodoNextweek,true);
+  
+  var_dump($TodoToday);
+  
 }
