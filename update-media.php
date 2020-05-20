fec58132c5bcacf05637080389f38bbd986de429
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-05-20 09:25:37 -0700

    Update update-media.php

--- update-media.php
+++ update-media.php
@@ -34,14 +34,6 @@ function UpdateMRR(){
   return;
 }
 
-UpdateMedia("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=628337542&format=image", "img/revenue_mix.jpg");
-UpdateMedia("https://docs.google.com/spreadsheets/d/e/2PACX-1vQWKMX0Yxi29TBATsQTDqcaPClYdnswQEExZwBO712WKwb9Ajnlq0DJsEcQUwxJNsgDORft9zw2gkqT/pubchart?oid=1236350701&format=image", "img/revenue_totals.jpg");
-
-UpdateMedia("https://graph.facebook.com/540290721/picture?width=9999&height=9999",  "cj.jpg");
-UpdateMedia("https://graph.facebook.com/540290721/picture?width=9999&height=9999",  "img/cj.jpg");
-UpdateMedia("https://graph.facebook.com/643280183/picture?width=9999&height=9999",  "img/ben.jpg");
-UpdateMedia("https://graph.facebook.com/1227603033/picture?width=9999&height=9999", "img/zach.jpg");
-UpdateMedia("https://graph.facebook.com/797969965/picture?width=9999&height=9999",  "img/jenny.jpg");
 
 UpdateMRR();
 
